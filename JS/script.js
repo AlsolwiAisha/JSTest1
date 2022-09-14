ecafebd26c10a6a6b50f803ea0f44ca53abbde5f
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-09-14 22:07:09 +0300

    Add C&F

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,20 @@ let z=x+y;
 
 console.log(z);//view in the console
 document.write(z); // view in my page
+
+//دالة حساب الفهرنهايت بدلالة الدرجة المئوية
+function F(){
+   
+    let cc=document.querySelector('#Ctxt').value;
+    let ff=(cc*1.8)+32;
+    document.querySelector('#f').innerHTML= cc+ "C is "+ff +" F";
+
+//دالة حساب الدرجة المئوية بدلالة الفهرنهايت
+}function C(){
+   
+    let ff=document.querySelector('#Ftxt').value;
+    let cc=(ff-32)/1.8;
+    document.querySelector('#c').innerHTML= ff+ "F is "+cc +" C";
+
+
+}
