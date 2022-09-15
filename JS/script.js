3d7156df2499bfa961bcef3843f8bf0881294e59
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-09-15 11:26:43 +0300

    update code

--- JS/script.js
+++ JS/script.js
@@ -6,19 +6,29 @@ let z=x+y;
 console.log(z);//view in the console
 document.write(z); // view in my page
 
-//دالة حساب الفهرنهايت بدلالة الدرجة المئوية
-function F(){
-   
-    let cc=document.querySelector('#Ctxt').value;
-    let ff=(cc*1.8)+32;
-    document.querySelector('#f').innerHTML= cc+ "C is "+ff +" F";
-
-//دالة حساب الدرجة المئوية بدلالة الفهرنهايت
-}function C(){
-   
-    let ff=document.querySelector('#Ftxt').value;
-    let cc=(ff-32)/1.8;
-    document.querySelector('#c').innerHTML= ff+ "F is "+cc +" C";
-
+var Class= document.getElementsByClassName('action');
+function checkC(){
+       Class[1].checked=false;
+  
+}
+function checkF(){
+        Class[0].checked=false;
 
 }
+function F() {
+    let v1=document.querySelector('#Ctxt').value;
+    let  v2,str1,str2;
+
+    if (Class[0].checked) {
+        v2=(v1*1.8)+32;//دالة حساب الفهرنهايت بدلالة الدرجة المئوية
+     str1=" C is ";
+     str2=" F";
+    }
+    
+    else {
+        v2=(v1-32)/1.8;//دالة حساب الدرجة المئوية بدلالة الفهرنهايت
+        str1=" F is ";
+        str2=" C";
+     }
+     document.querySelector('#f').innerHTML= v1+ str1+v2.toFixed(1) +str2;
+  }
