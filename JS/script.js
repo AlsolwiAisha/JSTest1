66c7db525c556b5648758ea1e4e86e3b1ab2d022
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-09-17 17:09:18 +0300

    Add calcuateDogAge function

--- JS/script.js
+++ JS/script.js
@@ -15,20 +15,28 @@ function checkF(){
         Class[0].checked=false;
 
 }
+//دالة حساب الدرجة المئويةوالفهرنهايت
 function F() {
     let v1=document.querySelector('#Ctxt').value;
-    let  v2,str1,str2;
+    let  v2,str1;
 
     if (Class[0].checked) {
         v2=(v1*1.8)+32;//دالة حساب الفهرنهايت بدلالة الدرجة المئوية
-     str1=" C is ";
-     str2=" F";
-    }
+     str1=`${v1} C is ${v2.toFixed(1)} F`;
+     }
     
     else {
         v2=(v1-32)/1.8;//دالة حساب الدرجة المئوية بدلالة الفهرنهايت
-        str1=" F is ";
-        str2=" C";
+        str1=`${v1} F is ${v2.toFixed(1)} C`;
      }
-     document.querySelector('#f').innerHTML= v1+ str1+v2.toFixed(1) +str2;
+     document.querySelector('#f').innerHTML= str1;
   }
+//دالة حساب عمر الكلب بالنسبة لعمر الانسان
+function calcuateDogAge(dogAge){
+    let doginfo={
+        _name:"bob",
+       age:Number(dogAge*7)
+    };
+console.log(`Your doggie is ${doginfo.age} years old in dog years`)
+}
+calcuateDogAge(5);
